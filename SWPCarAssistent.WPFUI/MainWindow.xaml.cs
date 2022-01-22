77d4c01fae7d481efdd1c20c703d1a0b390fff67
mwawrzyniiak <falers0@o2.pl>
2022-01-22 22:41:33 +0100

    begginig-of-the-implementation-of-dialogs-into-the-app

--- SWPCarAssistent.WPFUI/MainWindow.xaml.cs
+++ SWPCarAssistent.WPFUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Speech.AudioFormat;
+using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
 using SWPCarAssistent.Infrastructure.Repositories;
 
 namespace SWPCarAssistent
@@ -22,10 +25,35 @@ namespace SWPCarAssistent
     /// </summary>
     public partial class MainWindow : Window
     {
+        static SpeechSynthesizer ss;
+        static SpeechRecognitionEngine sre;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
             CarRepository cr = new CarRepository();
+
+            ConfigureSpeecher();
+        }
+
+        private void ConfigureSpeecher()
+        {
+            ss = new SpeechSynthesizer();
+            ss.SetOutputToDefaultAudioDevice();
+
+            var cultureInfo = new CultureInfo("pl-PL");
+            sre = new SpeechRecognitionEngine(cultureInfo);
+            sre.SetInputToDefaultAudioDevice();
+            sre.SpeechRecognized += Sre_SpeechRecognized;
+
+            ss.SpeakAsync("Witaj ponownie, od czego zaczynamy?");
+        }
+
+        private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
